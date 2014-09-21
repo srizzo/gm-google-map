9b7e6199e1bbc76634f479f98cf97defbef22707
srizzo <rizzolabs@gmail.com>
2014-09-21 19:38:48 +0100

    0.0.2

--- dist/gm-google-map.js
+++ dist/gm-google-map.js
@@ -1,4 +1,4 @@
-/*! gm-google-map - v0.0.1 - 2014-09-21
+/*! gm-google-map - v0.0.2 - 2014-09-21
 * https://github.com/srizzo/gm-google-map
 * Copyright (c) 2014 Samuel Rizzo; Licensed MIT */
 angular.module('gm-google-map', [])
