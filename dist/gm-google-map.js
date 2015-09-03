c117904a04bed9dc7acd7317b4c7979e8cb7dd40
srizzo <rizzolabs@gmail.com>
2015-09-03 18:38:20 +0200

    update protractor

--- dist/gm-google-map.js
+++ dist/gm-google-map.js
@@ -1,6 +1,6 @@
-/*! gm-google-map - v0.0.4 - 2014-11-18
+/*! gm-google-map - v0.0.4 - 2015-09-03
 * https://github.com/srizzo/gm-google-map
-* Copyright (c) 2014 Samuel Rizzo; Licensed MIT */
+* Copyright (c) 2015 Samuel Rizzo; Licensed MIT */
 angular.module('gm-google-map', [])
 /**
  * @description
